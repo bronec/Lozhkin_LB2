e3c38d2b355060276fb2724a2093bd004291247f
bronec <serzh.lozhkin2000@mail.ru>
2021-11-23 13:36:17 +0300

    add port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8012;
+const PORT = process.env.PORT || 8010;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
